d456571490e2d90a38293d684d9db0b213b89a36
Morgan Santamoor <msantamoor@gmail.com>
2020-02-18 18:10:44 -0700

    Trying to send Cookies to the UI domain

--- server.js
+++ server.js
@@ -119,11 +119,10 @@ app.get('/auth/google/callback',
         return res
         .status(200)
         .cookie('jwt', jwt.sign({exp: Math.floor(Date.now() / 1000) + (60 * 60), data: req.user._id}, process.env.signKey), {
-            expires: new Date(Date.now() + (signInTime)),
-            httpOnly: false,
             domain: process.env.FRONT_END_URL,
-            path:'/'
-
+            path:'/',
+            expires: new Date(Date.now() + (signInTime)),
+            httpOnly: false
         })
 
         .redirect(`${process.env.FRONT_END_URL}/Select`)

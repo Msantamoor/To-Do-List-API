61dfd9f708c0a52fbb760a7d3722cb32c66fd7db
Morgan Santamoor <msantamoor@gmail.com>
2020-02-18 17:59:17 -0700

    Domain specified

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get('/auth/google/callback',
         .cookie('jwt', jwt.sign({exp: Math.floor(Date.now() / 1000) + (60 * 60), data: req.user._id}, process.env.signKey), {
             expires: new Date(Date.now() + (signInTime)),
             httpOnly: false,
-            path: `${process.env.FRONT_END_URL}/`
+            domain: `${process.env.FRONT_END_URL}/`
         })
 
         .redirect(`${process.env.FRONT_END_URL}/Select`)

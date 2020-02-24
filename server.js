43e98b6576ab6dd3f5be7ede27a0192408de94a6
Morgan Santamoor <msantamoor@gmail.com>
2020-02-24 14:16:02 -0700

    Changed auth route back from async

--- server.js
+++ server.js
@@ -59,16 +59,17 @@ app.use(cors());
 
 passport.use(new GoogleStrategy(strategyOptions, verifyCallback))
   
-app.get('/auth/google', async (req, res) =>{
+app.get('/auth/google',
+// async (req, res) =>{
   passport.authenticate('google', {
       scope: [
           'https://www.googleapis.com/auth/userinfo.profile',
           'https://www.googleapis.com/auth/userinfo.email'
       ]
-  })
+    })
 //   res.send()
-})
-// .withCredentials = true
+)
+.withCredentials = true
 
 
 
